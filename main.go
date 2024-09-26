2c989d855b0e7f7e9f69a97622c1438c1db31628
Arp369 <vaghani.arpan@gmail.com>
2024-09-26 11:03:26 -0400

    update in code

--- main.go
+++ main.go
@@ -2,7 +2,21 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hello, World!")
-	fmt.Print("This one is without linebreak!")
+// func main() {
+// 	fmt.Println("Hello, World!")
+// 	fmt.Print("This one is without linebreak!")
+// }
+
+func main(){
+	Greeting()
+	advancedGreeting("Arpan")
+	advancedGreeting("V")
+}
+
+func Greeting(){
+	fmt.Println("Hey There!")
+}
+
+func advancedGreeting(name string){
+	fmt.Printf("Hello, %s\n", name)
 }
